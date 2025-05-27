af1475c30dcf1232ea11246850ad8a2eb8538161
Gabriel <gabriel.benini2@gmail.com>
2025-05-27 18:35:04 -0300

    Adicionando conexao com o banco de dados e privando informacoes pessoais

---
+++ src/main/java/com/gabriel/novo_gerenciador_tarefas/NovoGerenciadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.novo_gerenciador_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoGerenciadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoGerenciadorTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/novo_gerenciador_tarefas/NovoGerenciadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.novo_gerenciador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovoGerenciadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
